0017cacc41d35f525d4a1c7cd06abed1b40c4fb9
shun1121 <hikitanikun.ware80000@gmail.com>
2022-12-19 00:26:46 +0900

    refactor: fix pagination style

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,40 +1,23 @@
-import { createStyles } from '@mantine/core'
+import { useMantineColorScheme } from '@mantine/core'
+import clsx from 'clsx'
 import Link from 'next/link'
 import { Pagenation } from '../types/pagination'
 
-const useStyles = createStyles((theme) => ({
-  buttonWrapper: {
-    marginTop: '50px',
-    marginRight: '135px',
-  },
-  button: {
-    width: '120px',
-    height: '44px',
-    borderRadius: '5px',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3],
-    },
-  },
-  hideButton: {
-    visibility: 'hidden',
-  },
-}))
-
 export const Pagination = ({ currentPageNum, maxPageNum }: Pagenation) => {
-  const { classes } = useStyles()
+  const { colorScheme } = useMantineColorScheme()
+  const light = colorScheme === "light"
   const prevPage = currentPageNum - 1
   const nextPage = currentPageNum + 1
 
   return (
     <div className='mt-10 mb-10'>
       <div className='mx-auto flex items-center justify-between max-w-[930px]'>
-        <div className={currentPageNum !== 1 ? classes.button : classes.hideButton}>
+        <div className={currentPageNum !== 1 ? clsx("w-[120px] h-[44px] rounded-[5px] bg-dark-6 hover:bg-dark-4", {"bg-gray-10 hover:bg-gray-4": light}) : "invisible"}>
           <Link href={`/blog/page/${prevPage}`}>
             <a className='flex w-full h-full justify-center items-center font-bold'>戻る</a>
           </Link>
         </div>
-        <div className={currentPageNum !== maxPageNum ? classes.button : classes.hideButton}>
+        <div className={currentPageNum !== maxPageNum ? clsx("w-[120px] h-[44px] rounded-[5px] bg-dark-6 hover:bg-dark-4", {"bg-gray-10 hover:bg-gray-4": light}) : "invisible"}>
           <Link href={`/blog/page/${nextPage}`}>
             <a className='flex w-full h-full justify-center items-center font-bold'>次へ</a>
           </Link>
